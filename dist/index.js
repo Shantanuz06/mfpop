dfce84013a55c71d88a3c300d0f072415d81f12b
Shantanu Badgujar <shantanubadgujar2362002@gmail.com>
2024-04-30 16:15:48 +0530

    patched

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
-function add(x, y) {
-    return x + y;
+const luxon_1 = require("luxon");
+function formatDate(date, currentFormat, requiredFormat) {
+    const dt = luxon_1.DateTime.fromFormat(date.replace(/-/g, ' '), currentFormat.replace(/-/g, ' '));
+    return dt.toFormat(requiredFormat.replace(/-/g, ' ')).replace(/ /g, '-');
 }
-exports.add = add;
-const moment = require('moment');
-function formatDate(date, date_format, required_date_format) {
-    // Parse the input date using the specified format
-    const parsedDate = moment(date, date_format);
-    // Format the parsed date according to the required format
-    const formattedDate = parsedDate.format(required_date_format);
-    // Return the formatted date
-    return formattedDate;
-}
-// Example usage:
-const inputDate = "2001-05-01";
-const inputFormat = "YYYY-MM-DD";
-const outputFormat = "DD-MM-YYYY";
-const formattedDate = formatDate(inputDate, inputFormat, outputFormat);
-console.log(formattedDate); // Output: 01-05-2001
+exports.default = formatDate;
