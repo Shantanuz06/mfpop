fbe7d3ecf38ecf70771cf232256539f36316b0a2
Shantanu Badgujar <shantanubadgujar2362002@gmail.com>
2024-04-30 16:00:40 +0530

    update

--- index.ts
+++ index.ts
@@ -1,22 +1,8 @@
-const moment = require('moment');
+import { DateTime } from "luxon";
 
-
-
-export function formatDate(date:any, date_format:any, required_date_format:any) {
-    // Parse the input date using the specified format
-    const parsedDate = moment(date, date_format);
+export default function formatDate(date: string, currentFormat: string, requiredFormat: string): string {
     
-    // Format the parsed date according to the required format
-    const formattedDate = parsedDate.format(required_date_format);
-    
-    // Return the formatted date
-    return formattedDate;
-}
-
-// Example usage:
-const inputDate = "2001-05-01";
-const inputFormat = "YYYY-MM-DD";
-const outputFormat = "DD-MM-YYYY";
-
-const formattedDate = formatDate(inputDate, inputFormat, outputFormat);
-console.log(formattedDate); // Output: 01-05-2001
+    const dt = DateTime.fromFormat(date.replace(/-/g, ' '), currentFormat.replace(/-/g, ' '));
+   
+    return dt.toFormat(requiredFormat.replace(/-/g, ' ')).replace(/ /g, '-');
+}   
